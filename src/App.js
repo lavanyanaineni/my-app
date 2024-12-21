a863ae8db713b38a09cf42b9277d348834efbb32
Lavanya <lavanyanaineni@gmail.com>
2024-12-21 10:34:20 +0530

    adding all the files to the git

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Apple from './Apple';
+import Orange from './Orange';
+function App() {
+  return (
+    <div className="App">
+      <Apple name="LAVANYA" />
+      <Orange />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Orange.jsx
@@ -0,0 +1,8 @@
+function Orange()
+{
+    return(
+        <h1>Orange</h1>
+    )
+}
+
+export default Orange;
